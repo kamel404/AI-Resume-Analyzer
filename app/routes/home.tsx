392693814813c073cb88b3b10d5f427d3bf0928e
kamel404 <10121641@mu.edu.lb>
2025-07-31 12:24:56 +0300

    Implemented Authentication using Puter js Store

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import Navbar from "~/components/navbar";
 import type { Route } from "./+types/home";
 import ResumeCard from '../components/ResumeCard';
 import { resumes } from "constant";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 
 export function meta({ }: Route.MetaArgs) {
@@ -12,6 +15,16 @@ export function meta({ }: Route.MetaArgs) {
 }
 
 export default function Home() {
+
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth.isAuthenticated) {
+      navigate('auth?next=/');
+    }
+  }, [auth.isAuthenticated]);
+
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar />
     <section className="main-section">
